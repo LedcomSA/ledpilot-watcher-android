b3361656899baf78ce5e9156946e3252a90b7d15
lionel crasson <lionel@ledcom.be>
2022-08-29 13:31:58 +0200

    compat with android < 26

--- app/src/main/java/com/example/ledpilotwatcher/MainActivity.java
+++ app/src/main/java/com/example/ledpilotwatcher/MainActivity.java
@@ -14,18 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(new Intent(MainActivity.this,WatchService.class));
-       // }
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startService(new Intent(MainActivity.this,WatchService.class));
+        //}
         b_play =  (Button) findViewById(R.id.b_play);
         b_stop =  (Button) findViewById(R.id.b_stop);
 
         b_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    startForegroundService(new Intent(MainActivity.this,WatchService.class));
-                }
+                //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startService(new Intent(MainActivity.this,WatchService.class));
+                //}
             }
         });
         b_stop.setOnClickListener(new View.OnClickListener() {
